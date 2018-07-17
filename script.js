b6f35a317cc19446c62a1acc246c7dab300c2995
Tatsuya Nakajima <kuraudo.2039@gmail.com>
2018-07-17 09:29:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,63 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>在室状況管理システムHP</title>
-    <link rel="stylesheet" href="stylesheet_home.css">
-    <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet" >
-    <link href="https://fonts.googleapis.com/earlyaccess/hannari.css" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/earlyaccess/sawarabimincho.css" rel="stylesheet" />
-    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-    <script type="text/javascript" src="script.js"></script>
+$(function(){
+  //リアクションフォームのアコーディオン
+  $('.fa-arrow-circle-down').click(function(){
+    var $questionForm = $(this).parent().children('.question-form');
+    if($questionForm.hasClass('open')){
+      $questionForm.removeClass('open');
+      $questionForm.slideUp();
+    }
+    else{
+      $questionForm.addClass('open');
+      $questionForm.slideDown();
+    }
+  });
 
-  </head>
-  <body>
+  $('.tab').click(function(){
+    var $tab = $(this);
+    if($tab.hasClass('clicked')){
+      $tab.removeClass('clicked');
+     $('.type').css('display','none');
+    }
+    //ここの条件考える
+    else {
+      $('.type').removeClass('clicked');
+      $tab.removeClass('tab');
+  var tab=$tab.attr("class");
+      console.log(tab);
+  var a=$(".type").filter(".one").attr("class");
+        console.log(a);
+//      $('.type').filter(tab).addClass('clicked');
+      $tab.addClass('tab');
+//      $tab.addClass('clicked');
+//      $('.type').css('display','none');
+      if($('.type').hasClass($tab)){
 
-    <header>
-      <div class="container">
-        <img src="https://files.slack.com/files-pri/TB0DS5VSP-FB3VAUY0J/logo_trans.png" class="header-logo">
-        <div class="title">
-          <h1>在室状況管理システム</h1>
-          <h2>医療、介護の現場を支える</h2>
-        </div>
-        <div class="link">
-          <div class="menu">
-            <div class="home">
-              <a href="#"><i class="fas fa-home"></i>Home</a>
-            </div>
-            <div class="room">
-              <a href="file:///C:/Users/h27e25/Desktop/新しいフォルダー/condition.html"><i class="fas fa-search"></i>在室状況</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-    <main>
-      <div class="abstruct-wrapper">
-        <div class="container">
-          <div class="abstruct">
-            <h1 class="title">システム概要</h1>
-            <p>　在室状況管理とは、カメラで観測している部屋に人がいるかどうかを検知し、その状況をweb上に表示するシステムです。</p>
-          </div>
-        </div>
-      </div>
-    </main>
-    <footer>
-      <div class="helps-wrapper">
-        <div class="container">
-          <div class="question">
+      }
+    }
+  });
+/*
+  $('.tab-1').click(function(){
+    $('.type').css('display','none');
+    $('#mechanical').css('display','block');
 
-            <i class="fas fa-arrow-circle-down"></i>
-            <p class="helps-child">リアクション(改善案)</p>
-            <div class="question-form">
-              <p>意見内容</p>
-              <p>※本システムの改善点があれば、ここで意見を送信できます。</p>
-              <textarea class="question-textarea" name="name" rows="8" cols="80"></textarea>
-              <input class="question-submit" type="submit" name="" value="送信">
-            </div>
-          </div>
-<!--          <div class="help">
-            <i class="fas fa-arrow-circle-down"></i>
-            <p class="helps-child">ヘルプ</p>
-            <div class="help-form">
+  });
 
-            </div>
-          </div>
-!-->
-          <div class="footer-logo">
-            <h1>在室状況管理システム</h1>
-          </div>
-        </div>
-      </div>
-    </footer>
-  </body>
-</html>
+  $('.tab-2').click(function(){
+    $('.type').css('display','none');
+    $('#electronical').css('display','block');
+  });
+
+  $('.tab-3').click(function(){
+    $('.type').css('display','none');
+    $('#chemical').css('display','block');
+  });
+
+  $('.tab-4').click(function(){
+    $('.type').css('display','none');
+    $('#zivil').css('display','block');
+  });
+*/
+
+//ここからcondition.htmlの編集
+
+});
