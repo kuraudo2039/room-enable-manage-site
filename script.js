46bab0bfa251d505b36834454cf32cced6692d14
Tatsuya Nakajima <kuraudo.2039@gmail.com>
2018-07-17 09:27:54 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>在室状況管理システムHP</title>
+    <link rel="stylesheet" href="stylesheet_home.css">
+    <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet" >
+    <link href="https://fonts.googleapis.com/earlyaccess/hannari.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/earlyaccess/sawarabimincho.css" rel="stylesheet" />
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+
+  </head>
+  <body>
+
+    <header>
+      <div class="container">
+        <img src="https://files.slack.com/files-pri/TB0DS5VSP-FB3VAUY0J/logo_trans.png" class="header-logo">
+        <div class="title">
+          <h1>在室状況管理システム</h1>
+          <h2>医療、介護の現場を支える</h2>
+        </div>
+        <div class="link">
+          <div class="menu">
+            <div class="home">
+              <a href="#"><i class="fas fa-home"></i>Home</a>
+            </div>
+            <div class="room">
+              <a href="file:///C:/Users/h27e25/Desktop/新しいフォルダー/condition.html"><i class="fas fa-search"></i>在室状況</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <main>
+      <div class="abstruct-wrapper">
+        <div class="container">
+          <div class="abstruct">
+            <h1 class="title">システム概要</h1>
+            <p>　在室状況管理とは、カメラで観測している部屋に人がいるかどうかを検知し、その状況をweb上に表示するシステムです。</p>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer>
+      <div class="helps-wrapper">
+        <div class="container">
+          <div class="question">
+
+            <i class="fas fa-arrow-circle-down"></i>
+            <p class="helps-child">リアクション(改善案)</p>
+            <div class="question-form">
+              <p>意見内容</p>
+              <p>※本システムの改善点があれば、ここで意見を送信できます。</p>
+              <textarea class="question-textarea" name="name" rows="8" cols="80"></textarea>
+              <input class="question-submit" type="submit" name="" value="送信">
+            </div>
+          </div>
+<!--          <div class="help">
+            <i class="fas fa-arrow-circle-down"></i>
+            <p class="helps-child">ヘルプ</p>
+            <div class="help-form">
+
+            </div>
+          </div>
+!-->
+          <div class="footer-logo">
+            <h1>在室状況管理システム</h1>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
